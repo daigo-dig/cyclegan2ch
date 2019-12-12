c38ba37506b333bf2dd0d874b658e1473402b84b
rasukukunugi <s1611370@u.tsukuba.ac.jp>
2019-12-12 15:30:13 +0900

    first finish 2ch21ch cyclagen

--- cyclegan.py
+++ cyclegan.py
@@ -1,6 +1,3 @@
-from __future__ import print_function, division
-import scipy
-
 from keras.datasets import mnist
 from keras_contrib.layers.normalization.instancenormalization import InstanceNormalization
 from keras.layers import Input, Dense, Reshape, Flatten, Dropout, Concatenate
@@ -27,10 +24,9 @@ class CycleGAN():
         self.img_shapeB = (self.img_rows, self.img_cols,self.channelsB)
 
         # Configure data loader
-        self.dataset_name1 = 'apple2orange_mono'
-        self.dataset_name2 = 'apple2orange_mono'
-        self.data_loader = DataLoader(dataset_name1=self.dataset_name1,dataset_name2=self.dataset_name2,
-                                      img_res=(self.img_rows, self.img_cols))
+        self.dataset_name = 'apple2orange_mono'
+        self.data_loader = DataLoader(dataset_name=self.dataset_name,
+                                      img_rows=self.img_rows, img_cols=self.img_cols,img_channelsA=self.channelsA,img_channelsB=self.channelsB)
 
 
         # Calculate output shape of D (PatchGAN)
@@ -48,8 +44,8 @@ class CycleGAN():
         optimizer = Adam(0.0002, 0.5)
 
         # Build and compile the discriminators
-        self.d_A = self.build_discriminator()
-        self.d_B = self.build_discriminator()
+        self.d_A = self.build_discriminatorA()
+        self.d_B = self.build_discriminatorB()
         self.d_A.compile(loss='mse',
             optimizer=optimizer,
             metrics=['accuracy'])
@@ -63,12 +59,12 @@ class CycleGAN():
         #-------------------------
 
         # Build the generators
-        self.g_AB = self.build_generator()
-        self.g_BA = self.build_generator()
+        self.g_AB = self.build_generatorA()
+        self.g_BA = self.build_generatorB()
 
         # Input images from both domains
-        img_A = Input(shape=self.img_shape)
-        img_B = Input(shape=self.img_shape)
+        img_A = Input(shape=self.img_shapeA)
+        img_B = Input(shape=self.img_shapeB)
 
         # Translate images to the other domain
         fake_B = self.g_AB(img_A)
@@ -76,9 +72,6 @@ class CycleGAN():
         # Translate images back to original domain
         reconstr_A = self.g_BA(fake_B)
         reconstr_B = self.g_AB(fake_A)
-        # Identity mapping of images
-        img_A_id = self.g_BA(img_A)
-        img_B_id = self.g_AB(img_B)
 
         # For the combined model we will only train the generators
         self.d_A.trainable = False
@@ -91,17 +84,14 @@ class CycleGAN():
         # Combined model trains generators to fool discriminators
         self.combined = Model(inputs=[img_A, img_B],
                               outputs=[ valid_A, valid_B,
-                                        reconstr_A, reconstr_B,
-                                        img_A_id, img_B_id ])
+                                        reconstr_A, reconstr_B])
         self.combined.compile(loss=['mse', 'mse',
-                                    'mae', 'mae',
                                     'mae', 'mae'],
                             loss_weights=[  1, 1,
-                                            self.lambda_cycle, self.lambda_cycle,
-                                            self.lambda_id, self.lambda_id ],
+                                            self.lambda_cycle, self.lambda_cycle],
                             optimizer=optimizer)
 
-    def build_generator(self):
+    def build_generatorA(self):
         """U-Net Generator"""
 
         def conv2d(layer_input, filters, f_size=4):
@@ -122,7 +112,7 @@ class CycleGAN():
             return u
 
         # Image input
-        d0 = Input(shape=self.img_shape)
+        d0 = Input(shape=self.img_shapeA)
 
         # Downsampling
         d1 = conv2d(d0, self.gf)
@@ -136,11 +126,49 @@ class CycleGAN():
         u3 = deconv2d(u2, d1, self.gf)
 
         u4 = UpSampling2D(size=2)(u3)
-        output_img = Conv2D(self.channels, kernel_size=4, strides=1, padding='same', activation='tanh')(u4)
+        output_img = Conv2D(self.channelsB, kernel_size=4, strides=1, padding='same', activation='tanh')(u4)
+
+        return Model(d0, output_img)
+    def build_generatorB(self):
+        """U-Net Generator"""
+
+        def conv2d(layer_input, filters, f_size=4):
+            """Layers used during downsampling"""
+            d = Conv2D(filters, kernel_size=f_size, strides=2, padding='same')(layer_input)
+            d = LeakyReLU(alpha=0.2)(d)
+            d = InstanceNormalization()(d)
+            return d
+
+        def deconv2d(layer_input, skip_input, filters, f_size=4, dropout_rate=0):
+            """Layers used during upsampling"""
+            u = UpSampling2D(size=2)(layer_input)
+            u = Conv2D(filters, kernel_size=f_size, strides=1, padding='same', activation='relu')(u)
+            if dropout_rate:
+                u = Dropout(dropout_rate)(u)
+            u = InstanceNormalization()(u)
+            u = Concatenate()([u, skip_input])
+            return u
+
+        # Image input
+        d0 = Input(shape=self.img_shapeB)
+
+        # Downsampling
+        d1 = conv2d(d0, self.gf)
+        d2 = conv2d(d1, self.gf*2)
+        d3 = conv2d(d2, self.gf*4)
+        d4 = conv2d(d3, self.gf*8)
+
+        # Upsampling
+        u1 = deconv2d(d4, d3, self.gf*4)
+        u2 = deconv2d(u1, d2, self.gf*2)
+        u3 = deconv2d(u2, d1, self.gf)
+
+        u4 = UpSampling2D(size=2)(u3)
+        output_img = Conv2D(self.channelsA, kernel_size=4, strides=1, padding='same', activation='tanh')(u4)
 
         return Model(d0, output_img)
 
-    def build_discriminator(self):
+    def build_discriminatorA(self):
 
         def d_layer(layer_input, filters, f_size=4, normalization=True):
             """Discriminator layer"""
@@ -150,7 +178,28 @@ class CycleGAN():
                 d = InstanceNormalization()(d)
             return d
 
-        img = Input(shape=self.img_shape)
+        img = Input(shape=self.img_shapeA)
+
+        d1 = d_layer(img, self.df, normalization=False)
+        d2 = d_layer(d1, self.df*2)
+        d3 = d_layer(d2, self.df*4)
+        d4 = d_layer(d3, self.df*8)
+
+        validity = Conv2D(1, kernel_size=4, strides=1, padding='same')(d4)
+
+        return Model(img, validity)
+
+    def build_discriminatorB(self):
+
+        def d_layer(layer_input, filters, f_size=4, normalization=True):
+            """Discriminator layer"""
+            d = Conv2D(filters, kernel_size=f_size, strides=2, padding='same')(layer_input)
+            d = LeakyReLU(alpha=0.2)(d)
+            if normalization:
+                d = InstanceNormalization()(d)
+            return d
+
+        img = Input(shape=self.img_shapeB)
 
         d1 = d_layer(img, self.df, normalization=False)
         d2 = d_layer(d1, self.df*2)
@@ -200,7 +249,6 @@ class CycleGAN():
                 # Train the generators
                 g_loss = self.combined.train_on_batch([imgs_A, imgs_B],
                                                         [valid, valid,
-                                                        imgs_A, imgs_B,
                                                         imgs_A, imgs_B])
 
                 elapsed_time = datetime.datetime.now() - start_time
@@ -219,12 +267,22 @@ class CycleGAN():
                 # If at save interval => save generated image samples
                 if batch_i % sample_interval == 0:
                     self.sample_images(epoch, batch_i)
+                if batch_i % (sample_interval*10) == 0:
+                    self.sample_models(epoch, batch_i)
+
+    def sample_models(self, epoch, batch_i):
+        os.makedirs('saved_model/%s' % self.dataset_name,exist_ok=True)
+        self.g_AB.save_weights("saved_model/%s/%d_%d.hdf5" % (self.dataset_name, epoch, batch_i))
+        json_string = self.g_AB.to_json()
+        open("saved_model/%s/%d_%d.json" % (self.dataset_name, epoch, batch_i),"w").write(json_string)
 
     def sample_images(self, epoch, batch_i):
         os.makedirs('images/%s' % self.dataset_name, exist_ok=True)
-        r, c = 2, 3
+        r, c = 2, 6
 
-        imgs_A = self.data_loader.load_data(domain="A", batch_size=1, is_testing=True)
+        #imgs_A1 = self.data_loader.load_data(domain="A1", batch_size=1, is_testing=True)
+        #imgs_A2 = self.data_loader.load_data(domain="A2", batch_size=1, is_testing=True)
+        imgs_A = self.data_loader.load_dataA(domain="A", batch_size=1, is_testing=True)
         imgs_B = self.data_loader.load_data(domain="B", batch_size=1, is_testing=True)
 
         # Demo (for GIF)
@@ -238,20 +296,45 @@ class CycleGAN():
         reconstr_A = self.g_BA.predict(fake_B)
         reconstr_B = self.g_AB.predict(fake_A)
 
+        imgs_A = np.reshape(imgs_A,(2,256,256))
+        fake_B = np.reshape(fake_B,(1,256,256))
+        reconstr_A = np.reshape(reconstr_A,(2,256,256))
+        imgs_B = np.reshape(imgs_B,(1,256,256))
+        fake_A = np.reshape(fake_A,(2,256,256))
+        reconstr_B = np.reshape(reconstr_B,(1,256,256))
+
+        white = np.zeros((256,256))
+        white = white + 255
+
         gen_imgs = np.concatenate([imgs_A, fake_B, reconstr_A, imgs_B, fake_A, reconstr_B])
 
-        gen_imgs = np.reshape(gen_imgs,(6,256,256))
+        gen_imgs = np.reshape(gen_imgs,(9,256,256))
 
         # Rescale images 0 - 1
         gen_imgs = 0.5 * gen_imgs + 0.5
 
-        titles = ['Original', 'Translated', 'Reconstructed']
+        titles = ['O1', 'O2', 'T1', 'T2', 'R1', 'R2']
         fig, axs = plt.subplots(r, c)
         cnt = 0
         for i in range(r):
             for j in range(c):
+                if i == 0 and j == 3:
+                    axs[i,j].imshow(white,cmap="gray")
+                    axs[i,j].set_title(titles[j])
+                    axs[i,j].axis('off')
+                    continue
+                if i == 1 and j == 1:
+                    axs[i,j].imshow(white,cmap="gray")
+                    axs[i,j].set_title(titles[j])
+                    axs[i,j].axis('off')
+                    continue
+                if i == 1 and j == 5:
+                    axs[i,j].imshow(white,cmap="gray")
+                    axs[i,j].set_title(titles[j])
+                    axs[i,j].axis('off')
+                    continue
                 axs[i,j].imshow(gen_imgs[cnt],cmap="gray")
-                axs[i, j].set_title(titles[j])
+                axs[i,j].set_title(titles[j])
                 axs[i,j].axis('off')
                 cnt += 1
         fig.savefig("images/%s/%d_%d.png" % (self.dataset_name, epoch, batch_i))
--- data_loader.py
+++ data_loader.py
@@ -5,14 +5,42 @@ import cv2
 from operator import itemgetter
 
 class DataLoader():
-    def __init__(self, dataset_name,img_res=(256, 256)):
+    def __init__(self, dataset_name,img_rows=256,img_cols=256,img_channelsA=2,img_channelsB=1):
         self.dataset_name = dataset_name
-        self.img_res = img_res
+        self.img_rows = img_rows
+        self.img_cols = img_cols
+        self.img_channelsA = img_channelsA
+        self.img_channelsB = img_channelsB
+        self.img_res = (img_rows,img_cols)
+        self.img_shapeA = (img_rows,img_cols,img_channelsA)
+        self.img_shapeB = (img_rows,img_cols,img_channelsB)
 
     def load_data(self, domain, batch_size=1, is_testing=False):
         data_type = "train%s" % domain if not is_testing else "test%s" % domain
         path = glob('./datasets/%s/%s/*' % (self.dataset_name, data_type))
-        data_size = len(path)
+
+        batch_images = np.random.choice(path, size=batch_size)
+
+        imgs = []
+        for img_path in batch_images:
+            img = self.imread(img_path)
+            if not is_testing:
+                img = np.reshape(cv2.resize(img, self.img_res),self.img_shapeB)
+                if np.random.random() > 0.5:
+                    img = np.fliplr(img)
+            else:
+                img = np.reshape(cv2.resize(img, self.img_res),self.img_shapeB)
+            imgs.append(img)
+
+        imgs = np.array(imgs)/127.5 - 1.
+
+        return imgs
+
+    def load_dataA(self, domain, batch_size=1, is_testing=False):
+        data_type = "train%s" % domain if not is_testing else "test%s" % domain
+        path1 = glob('./datasets/%s/%s%s/*' % (self.dataset_name, data_type, "1"))
+        path2 = glob('./datasets/%s/%s%s/*' % (self.dataset_name, data_type, "2"))
+        data_size = min(len(path1), len(path2))
         shuffled_idx = np.random.choice(np.arange(data_size), batch_size, replace=False)
 
         imgs = []
@@ -21,11 +49,11 @@ class DataLoader():
             img2 = self.imread(path2[img_path])
             img = np.stack([img1,img2])
             if not is_testing:
-                img = np.reshape(cv2.resize(img, self.img_res),(256,256,1))
+                img = np.reshape(img,self.img_shapeA)
                 if np.random.random() > 0.5:
                     img = np.fliplr(img)
             else:
-                img = np.reshape(cv2.resize(img, self.img_res),(256,256,1))
+                img = np.reshape(img,self.img_shapeA)
             imgs.append(img)
 
         imgs = np.array(imgs)/127.5 - 1.
@@ -34,7 +62,7 @@ class DataLoader():
 
     def load_batch(self, batch_size=1, is_testing=False):
         data_type = "train" if not is_testing else "val"
-        path_A1 = glob('./datasets/%s/%sA/*' % (self.dataset_name, data_type))
+        path_A1 = glob('./datasets/%s/%sA1/*' % (self.dataset_name, data_type))
         path_A2 = glob('./datasets/%s/%sA2/*' % (self.dataset_name, data_type))
         path_B = glob('./datasets/%s/%sB/*' % (self.dataset_name, data_type))
 
@@ -62,8 +90,8 @@ class DataLoader():
                 img_B = self.imread(img_B)
                 img_A = np.stack([img_A1,img_A2])
 
-                img_A = np.reshape(img_A,(256,256,2))
-                img_B = np.reshape(cv2.resize(img_B, self.img_res),(256,256,1))
+                img_A = np.reshape(img_A,self.img_shapeA)
+                img_B = np.reshape(cv2.resize(img_B, self.img_res),self.img_shapeB)
 
                 if not is_testing and np.random.random() > 0.5:
                         img_A = np.fliplr(img_A)
